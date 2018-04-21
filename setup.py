63eda56281e9a35218f6adaa6ff9fc976acf3706
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-04-22 01:06:04 +0900

    Bump version number to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 __author__ = 'Yusuke Miyazaki <miyazaki.dev@gmail.com>'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 here = path.abspath(path.dirname(__file__))
 
